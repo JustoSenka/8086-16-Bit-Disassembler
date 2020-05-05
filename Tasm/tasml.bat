0718ac0ae8e8f3c1a717928ae6123ee807736365
Justas <glodjus@gmail.com>
2020-05-05 22:42:10 +0200

    Adding TASM for easeir compillation of assembler code

---
+++ Tasm/tasml.bat
@@ -0,0 +1,29 @@
+@echo off
+cd\
+cd tasm
+tasm
+cls
+Echo.
+@echo off
+Echo  +=================================================================+
+Echo   Power Programming Tools 64 Bit By Chaitanya Patel : Techapple.Net
+Echo  +=================================================================+
+Echo ____________________________________________________________________
+Echo  Type Proper Command To Perform the Desired Action
+Echo ____________________________________________________________________
+Echo  Command 		.	Action
+Echo 			.
+ECHO  Edit		-	Open MS-DOS Editor
+ECHO  TASM 		-	Compilation
+Echo  tlink		-       Perform Linking
+Echo  td		-       Launch Turbo Debugger
+Echo  Exit		-	Exit Tasm 1.2
+Echo _____________________________________________________________________
+ECHO  Notes ---> Tasm Commands Are same as 32 Bit Version
+ECHO 	For Compiling your files tasm "yourfilename".asm use without quotes
+Echo	e.g for compiling add.asm command is : tasm add.asm
+Echo	For Linking and debugging same as 32 bit : tlink,td
+Echo    .
+Echo    Complink,DPMIload and TasmX also available using 32bit commands
+Echo _____________________________________________________________________
+ECHO.
